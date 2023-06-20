4b7487076aeac350dade99e0a34a5e2b4df790c8
Musa Yohanes <musayohanes00@gmail.com>
2023-06-20 20:40:02 +0700

    feature: use API autoparse and add json parse
    - Create parse_json function for API autoparse response
    - Modify API request payload
    - Add hl:en param in google url
    - Change query for test purpose

--- google_scraper/google_scraper/spiders/google_serp.py
+++ google_scraper/google_scraper/spiders/google_serp.py
@@ -1,5 +1,6 @@
 import scrapy
 import logging
+import json
 from datetime import datetime
 from google_scraper.items import GoogleSearchResult
 from os import environ
@@ -12,7 +13,7 @@ load_dotenv()
 # Prepare API request URL for proxy connection
 # Request URL must use allowed domain only
 def get_url(url):
-    payload = {"api_key": environ.get("SCRAPEOPS_API_KEY"), "url": url}
+    payload = {"api_key": environ.get("SCRAPEOPS_API_KEY"), "auto_extract": "google", "url": url}
     proxy_url = "https://proxy.scrapeops.io/v1/?" + urlencode(payload)
     
     logging.info("Final URL: %s", proxy_url)
@@ -23,6 +24,7 @@ def create_google_url(query, num=100, start=0):
     url = {
         "q": query,
         "num": num,
+        "hl": "en",
         "start": start
     }
     return "https://www.google.com/search?" + urlencode(url)
@@ -57,26 +59,26 @@ class GoogleSerpSpider(scrapy.Spider):
 
     # Function call when spider crawl request initiated
     def start_requests(self):
-        queries = ["naruto"]
-        scraped_items = 0
+        queries = ["apple fact"]
+        current_page = 0
         ITEMS_PER_REQUEST = 100
-        DESIRED_ITEMS = 200
+        TARGET_PAGE = 2
         
         if not environ.get("SCRAPER_API_KEY") or not environ.get("SCRAPEOPS_API_KEY"):
             raise RuntimeError("API_KEY not set")
         for query in queries:
             url = create_google_url(query, num=ITEMS_PER_REQUEST)
-            yield scrapy.Request(url=get_url(url), callback=self.parse, headers=self.headers, meta={"scraped_items": scraped_items, 
+            yield scrapy.Request(url=get_url(url), callback=self.parse_json, headers=self.headers, meta={"current_page": current_page, 
                                                                                                     "query": query, 
-                                                                                                    "DESIRED_ITEMS": DESIRED_ITEMS, 
+                                                                                                    "TARGET_PAGE": TARGET_PAGE, 
                                                                                                     "ITEMS_PER_REQUEST": ITEMS_PER_REQUEST})
 
     # Function to parse response obtained
     def parse(self, response):
         query = response.meta["query"]
-        scraped_items = response.meta["scraped_items"]
+        current_page = response.meta["current_page"]
         ITEMS_PER_REQUEST = response.meta["ITEMS_PER_REQUEST"]
-        DESIRED_ITEMS = response.meta["DESIRED_ITEMS"]
+        TARGET_PAGE = response.meta["TARGET_PAGE"]
 
         dt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         item = GoogleSearchResult()
@@ -91,11 +93,43 @@ class GoogleSerpSpider(scrapy.Spider):
             
             yield item
 
-        # Call request to next page if desired items to be scraped ("DESIRED_ITEMS") not yet scraped
-        scraped_items += ITEMS_PER_REQUEST
-        if scraped_items < DESIRED_ITEMS:
-            url = create_google_url(query, num=ITEMS_PER_REQUEST, start=scraped_items)
-            yield scrapy.Request(url=get_url(url), callback=self.parse, headers=self.headers, meta={"scraped_items": scraped_items, 
+        # Call request to next page if desired items to be scraped ("TARGET_PAGE") not yet scraped
+        current_page += 1
+        if current_page < TARGET_PAGE:
+            url = create_google_url(query, num=ITEMS_PER_REQUEST, start=current_page*ITEMS_PER_REQUEST)
+            yield scrapy.Request(url=get_url(url), callback=self.parse, headers=self.headers, meta={"current_page": current_page, 
                                                                                                     "query": query, 
-                                                                                                    "DESIRED_ITEMS": DESIRED_ITEMS, 
+                                                                                                    "TARGET_PAGE": TARGET_PAGE, 
                                                                                                     "ITEMS_PER_REQUEST": ITEMS_PER_REQUEST})
+    
+    # Function to parse API autoparse response (json) obtained
+    def parse_json(self, response):
+        query = response.meta["query"]
+        current_page = response.meta["current_page"]
+        ITEMS_PER_REQUEST = response.meta["ITEMS_PER_REQUEST"]
+        TARGET_PAGE = response.meta["TARGET_PAGE"]
+
+        dt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        item = GoogleSearchResult()
+        results = json.loads(response.text)
+        for box in results["data"]["organic_results"]:
+            item["title"] = box.get("title","Empty"),
+            item["url"] = box.get("link","Empty"),
+            item["text"] = box.get("snippet","Empty"),
+            item["datetime"] = dt
+            
+            yield item
+
+        # Call request to next page if desired pages to be scraped ("TARGET_PAGE") not yet scraped
+        current_page += 1
+        if current_page < TARGET_PAGE:
+            next_page = results["data"]["pagination"].get("next_page_url")
+            if next_page:
+                yield scrapy.Request(url=get_url(next_page), callback=self.parse_json, headers=self.headers, meta={"current_page": current_page, 
+                                                                                                                    "query": query, 
+                                                                                                                    "TARGET_PAGE": TARGET_PAGE, 
+                                                                                                                    "ITEMS_PER_REQUEST": ITEMS_PER_REQUEST})
+            else:
+                logging.info("No next page to be scraped, current_page:%s, pages_count:%s", current_page, results["data"]["pagination"].get("pages_count"))
+        else:
+            logging.info("Target page reached, current_page:%s, target_page:%s, pages_count:%s", current_page, TARGET_PAGE, results["data"]["pagination"].get("pages_count"))
